edd7ddc32a5c690035a16881ce99fc9b260eddad
Shihab <shihabul.islam.shihab.004@gmail.com>
2026-06-03 20:57:13 +0600

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int max_p=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(j>i){
+                    int profit=prices[j]-prices[i];
+                    max_p=max(profit,max_p);
+                }
+            }
+        }
+
+        return max_p;
+    }
+};
