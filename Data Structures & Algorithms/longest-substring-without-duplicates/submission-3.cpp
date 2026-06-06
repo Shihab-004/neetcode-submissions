db49fecd84dc4a78103f0d6f9d897c4535eb1d00
Shihab <shihabul.islam.shihab.004@gmail.com>
2026-06-06 12:44:41 +0600

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0,r=0,max_len=0;
+        unordered_set<char>st;
+        while(r<s.size()){
+            if(st.find(s[r]) == st.end()){ // repeate khuje na pai 
+                st.insert(s[r]);
+                int len=st.size();
+                max_len=max(max_len,len);
+                r++;
+            }
+            else {
+                st.erase(s[l]);
+                int len=st.size();
+                // max_len=max(max_len,len); // dorkar nai eta,, shrink hoile upate korar ...
+                l++;
+            }
+        }
+        return max_len;
+    }
+};
